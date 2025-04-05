545343f6f7a1997fc1cfbbb2ef588c485ebcad6c
mtrapaglia <matiasdmansilla@hotmail.com>
2025-04-05 02:41:39 -0300

    position coded

--- main.lua
+++ main.lua
@@ -5,8 +5,8 @@ function love.load()
     anchoVentana, altoVentana = love.graphics.getDimensions()
     for _ = 1, 3 do
         local nueva_bola = entities.new()
-        nueva_bola.x = math.random(nueva_bola.radio, anchoVentana - nueva_bola.radio)
-        nueva_bola.y = math.random(nueva_bola.radio, altoVentana - nueva_bola.radio)
+        nueva_bola.position[1] = math.random(nueva_bola.radio, anchoVentana - nueva_bola.radio)
+        nueva_bola.position[2] = math.random(nueva_bola.radio, altoVentana - nueva_bola.radio)
         table.insert(bolas, nueva_bola)
     end
 end
@@ -15,7 +15,7 @@ function love.mousepressed(x, y, button)
     local bola_selected = false
     if button == 1 then  
         for _, bola in ipairs(bolas) do
-            local distancia = math.sqrt((x - bola.x)^2 + (y - bola.y)^2)
+            local distancia = math.sqrt((x - bola.position[1])^2 + (y - bola.position[2])^2)
             if distancia <= bola.radio then
                 for _, otraBola in ipairs(bolas) do
                     otraBola.selected = false
@@ -44,11 +44,11 @@ function love.draw()
 
     for _, bola in ipairs(bolas) do
         love.graphics.setColor(bola.color)
-        love.graphics.circle("fill", bola.x, bola.y, bola.radio)
+        love.graphics.circle("fill", bola.position[1], bola.position[2], bola.radio)
         
         if bola.selected then
             love.graphics.setColor(1, 1, 1) -- white
-            love.graphics.circle("line", bola.x, bola.y, bola.radio + 4)
+            love.graphics.circle("line", bola.position[1], bola.position[2], bola.radio + 4)
         end
     end
 end
--- src/entities.lua
+++ src/entities.lua
@@ -1,14 +1,18 @@
 require "conf"
 
+--- @class bola
+--- @field color table
+--- @field radio number
+--- @field position table
+--- @field selected boolean
+--- @field target_location
 local bola = {}
 
 function bola.new()
     local self = {}
     self.color = {math.random(0, 255)/255, math.random(0, 255)/255, math.random(0, 255)/255}
     self.radio = math.random(15, 30)
-    -- x e y se establecerán después de crear el objeto
-    self.x = 0
-    self.y = 0
+    self.position = {0, 0}
     self.selected = false
     return self
 end
