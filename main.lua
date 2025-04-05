2be71f048e29623f7a997afd9e382b3e0350dc02
mtrapaglia <matiasdmansilla@hotmail.com>
2025-04-05 02:56:21 -0300

    cambiamos velocidad por speed

--- main.lua
+++ main.lua
@@ -1,18 +1,20 @@
 local entities = require "src/entities"
 
-bolas = {}
+local bola_selected = nil
+local bolas = {}
 function love.load()
     anchoVentana, altoVentana = love.graphics.getDimensions()
     for _ = 1, 3 do
         local nueva_bola = entities.new()
         nueva_bola.position[1] = math.random(nueva_bola.radio, anchoVentana - nueva_bola.radio)
         nueva_bola.position[2] = math.random(nueva_bola.radio, altoVentana - nueva_bola.radio)
+        nueva_bola.target_location[1] = nueva_bola.position[1]
+        nueva_bola.target_location[2] = nueva_bola.position[2]
         table.insert(bolas, nueva_bola)
     end
 end
 
 function love.mousepressed(x, y, button)
-    local bola_selected = false
     if button == 1 then  
         for _, bola in ipairs(bolas) do
             local distancia = math.sqrt((x - bola.position[1])^2 + (y - bola.position[2])^2)
@@ -21,7 +23,7 @@ function love.mousepressed(x, y, button)
                     otraBola.selected = false
                 end
                 bola.selected = true
-                bola_selected = true
+                bola_selected = bola
                 break
             end
         end
@@ -31,11 +33,28 @@ function love.mousepressed(x, y, button)
             end
         end
     elseif button == 2 and bola_selected then
-        -- TODO: Implementar acción con click derecho
+        bola_selected.target_location[1] = x
+        bola_selected.target_location[2] = y
     end
 end
 
-function love.update()
+function love.update(dt)
+    if bola_selected then
+        local distancia = math.sqrt(
+            (bola_selected.target_location[1] - bola_selected.position[1])^2 + 
+            (bola_selected.target_location[2] - bola_selected.position[2])^2
+        )
+        
+        if distancia > 1 then  -- Si está a más de 1 píxel de distancia
+            -- Calculamos la dirección normalizada
+            local dx = (bola_selected.target_location[1] - bola_selected.position[1]) / distancia
+            local dy = (bola_selected.target_location[2] - bola_selected.position[2]) / distancia
+            
+            -- Aplicamos la velocidad considerando el delta time
+            bola_selected.position[1] = bola_selected.position[1] + dx * bola_selected.speed * dt
+            bola_selected.position[2] = bola_selected.position[2] + dy * bola_selected.speed * dt
+        end
+    end
 end
 
 function love.draw()
--- src/entities.lua
+++ src/entities.lua
@@ -5,7 +5,8 @@ require "conf"
 --- @field radio number
 --- @field position table
 --- @field selected boolean
---- @field target_location
+--- @field target_location table
+--- @field speed number
 local bola = {}
 
 function bola.new()
@@ -13,7 +14,9 @@ function bola.new()
     self.color = {math.random(0, 255)/255, math.random(0, 255)/255, math.random(0, 255)/255}
     self.radio = math.random(15, 30)
     self.position = {0, 0}
+    self.target_location = {0, 0}
     self.selected = false
+    self.speed = 200  -- velocidad en píxeles por segundo
     return self
 end
 
